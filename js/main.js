9a7d6be96ab4701d7fcda501c09682eb56b3c8dd
LinoGitas <63246130+LinoGitas@users.noreply.github.com>
2020-05-18 12:32:52 +0300

    update

--- js/main.js
+++ js/main.js
@@ -1,24 +1,23 @@
-'use strict';
+"use strict";
 
-// OOP -object oriented programming ( objektinis programavimas )
+// OOP - object oriented programming (objektinis programavimas)
 
 // class - pagrindinis dalykas (receptas)
-// oject - class'es gaminys (pyragas)
+// object - class'es gaminys (pyragas)
 
 // params:
 // resolution
 // ratio
 // color
 // remote: true/false
-// type flat / CRT
+// type: flat / CRT
 
-// metodai: kagalima su juo daryti?
-// ijungti / isjungti
+// metodai: ka galima su juo daryti?
+// ijungti/isjungti
 // pakeisti kanala
 // keisti garsa
-
-class Tv { 
-    constructor(name, resolution, ratio, color, remote, type ) {
+class Tv {
+    constructor( name, resolution, ratio, color, remote, type ) {
         this.name = name;
         this.resolution = resolution;
         this.ratio = ratio;
@@ -27,29 +26,61 @@ class Tv {
         this.type = type;
         this.turnedOn = false;
         this.channel = '';
-        this.volume = 0;
+        this.volume = 20;
     }
 
-    turnOn( params ) {
+    turnOn() {
+        if ( this.turnedOn ) {
+            console.log(this.name + ' already is turned on.');
+            return;
+        }
         this.turnedOn = true;
-
-        console.log(this.name + ' has neen turn on.');
+        console.log(this.name + ' has been turned on.');
     }
 
-    turnOff( params ) {
+    turnOff() {
+        if ( !this.turnedOn ) {
+            console.log(this.name + ' already is turned off.');
+            return;
+        }
         this.turnedOn = false;
-        console.log(this.name + ' has neen turn off.');
+        console.log(this.name + ' has been turned off.');
+    }
+
+    changeVolume( levelChange ) {
+        this.volume += levelChange;
+        if ( this.volume > 100 ) {
+            this.volume = 100;
+        }
+        if ( this.volume < 0 ) {
+            this.volume = 0;
+        }
+        console.log(`${this.name} volume changed to ${this.volume}.`);
+    }
+
+    currentVolume() {
+        console.log(`Current volume for ${this.name} is ${this.volume}.`);
+    }
+
+    mute() {
+        this.volume = 0;
+        console.log(`${this.name} has been muted.`);
     }
 }
 
-const silelis = new Tv('silelis', [4000, 3000], [4, 3], 'black', false, 'CRT');
+const silelis = new Tv('Silelis', [4000, 3000], [4, 3], 'black', false, 'CRT');
 console.log( silelis );
 
-const lg = new Tv('lg',[8000, 6000], [4, 3], 'silver', true, 'flat');
+const lg = new Tv('LG', [8000, 6000], [4, 3], 'silver', true, 'flat');
 console.log( lg );
 
-silelis.turnOn();
-silelis.turnOff();
+// silelis.turnOff();
+// silelis.turnOn();
+// silelis.turnOff();
 
 lg.turnOn();
+lg.changeVolume(30);
 lg.turnOff();
+lg.turnOn();
+lg.currentVolume();
+lg.mute();
